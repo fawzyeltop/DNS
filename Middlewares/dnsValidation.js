f977ac88e61d75b8d04157af8009f4b0a76cb680
Your Name <you@example.com>
2021-03-24 16:10:56 +0200

    asdasdsada

--- Middlewares/dnsValidation.js
+++ Middlewares/dnsValidation.js
@@ -2,22 +2,22 @@ const validator = require('../Utils/validation');
 
 const dnsValidation = (req, res, next) => {
     const validationRule = {
-        "A": "required|string",
-        "AAAA": "required|string",
-        "CNAME": "required|string",
+        "A": "required|string|IPv4Strict",
+        "AAAA": "required|string|IPv6Strict",
+        "CNAME": "required|string|CNAMEStrict",
         "MX": "required|string",
         "NS": "required|string",
         "PTR": "required|string",
         "SOA": "required|string",
-        "SRV": "required|string",
+        "SRV": "required|string|SRVStrict",
         "TXT": "required|string",
     }
     validator(req.body, validationRule, {}, (err, status) => {
         if (!status) {
             res.status(412).json({
+                 statusCode: 412,
                  status: 'Validation Error',
                  message: err
-                
             });
         } else {
             next();
--- Utils/validation.js
+++ Utils/validation.js
@@ -1,8 +1,26 @@
 const Validator = require('validatorjs');
-Validator.useLang('ar');
+
+const IPv4 = /^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$/;
+Validator.register('IPv4Strict', value => IPv4.test(value),
+    'IPv4 must be valid');
+
+const IPv6 = /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/;
+Validator.register('IPv6Strict', value => IPv6.test(value),
+'IPv6 must be valid');
+
+const SRV = /\b((xn--)?[a-z0-9\w]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b/;
+Validator.register('SRVStrict', value => SRV.test(value),
+'SRV must be valid');
+
+
+const CNAME = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
+Validator.register('CNAMEStrict', value => CNAME.test(value),
+'CNAME must be valid');
+
 const validator = (body, rules, customMessages, callback) => {
     const validation = new Validator(body, rules, customMessages);
     validation.passes(() => callback(null, true));
     validation.fails(() => callback(validation.errors, false));
 };
+
 module.exports = validator;
